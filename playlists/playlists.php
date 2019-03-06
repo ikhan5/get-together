56d7747aea8c0aee079dc4c5913819d7539dca9d
Imzan Khan <ikhan5@lakeheadu.ca>
2019-03-06 18:52:58 -0500

    Tablet Interface Updated

--- playlists/playlists.php
+++ playlists/playlists.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Playlists</title>
-    <link rel="stylesheet" href="CSS/playlists.css">
+    <link rel="stylesheet" href="../CSS/playlists.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
@@ -29,51 +29,55 @@
             </a>
         </div>
     </header>
-    <!--Playlist Listings-->
-    <section id="playlists">
-        <div class="playlist">
-            <div class="playlist_item playlist__image">
-                <i class="fas fa-headphones-alt playlist-icon"></i>
-            </div>
-            <a href="#" class="playlist_item playlist__info">
-                <div>
-                    <p class="playlist__title">Playlist Title</p>
-                    <p class="playlist__song-count">Number of Songs</p>
+    <div class="playlists__layout">
+        <!--Playlist Listings-->
+        <section id="playlists">
+            <div class="playlist">
+                <div class="playlist_item playlist__image">
+                    <i class="fas fa-headphones-alt playlist-icon"></i>
                 </div>
-            </a>
-            <div class="playlist_item playlist__options"> <a href="#"><i class="fas fa-ellipsis-v"></i></a></div>
-        </div>
-    </section>
-    <!--Song Listings for each Playlist-->
-    <h2>Songs</h2>
-    <section id="songs" class="sortable">
-        <div class="song">
-            <div class="song_item song__image">
-                <i class="fas fa-headphones-alt playlist-icon"></i>
+                <a href="#" class="playlist_item playlist__info">
+                    <div>
+                        <p class="playlist__title">Playlist Title</p>
+                        <p class="playlist__song-count">Number of Songs</p>
+                    </div>
+                </a>
+                <div class="playlist_item playlist__options"> <a href="#"><i class="fas fa-ellipsis-v"></i></a></div>
             </div>
-            <a href="#" class="song_item song__info">
-                <div>
-                    <p class="song__title">Playlist Title 1</p>
-                    <p class="song__artist">Artist</p>
-                    <p class="song__time">3:00</p>
-                </div>
-            </a>
-            <div class="song_item song__options"> <a href="#"><i class="fas fa-ellipsis-v"></i></a></div>
+        </section>
+        <div class="divider">
         </div>
-        <div class="song">
-            <div class="song_item song__image">
-                <i class="fas fa-headphones-alt playlist-icon"></i>
+        <!--Song Listings for each Playlist-->
+        <section id="songs" class="sortable">
+            <h2>Songs</h2>
+            <div class="song">
+                <div class="song_item song__image">
+                    <i class="fas fa-headphones-alt playlist-icon"></i>
+                </div>
+                <a href="#" class="song_item song__info">
+                    <div>
+                        <p class="song__title">Song Title 1</p>
+                        <p class="song__artist">Artist</p>
+                        <p class="song__time">3:00</p>
+                    </div>
+                </a>
+                <div class="song_item song__options"> <a href="#"><i class="fas fa-ellipsis-v"></i></a></div>
             </div>
-            <a href="#" class="song_item song__info">
-                <div>
-                    <p class="song__title">Playlist Title 2</p>
-                    <p class="song__artist">Artist</p>
-                    <p class="song__time">3:00</p>
+            <div class="song">
+                <div class="song_item song__image">
+                    <i class="fas fa-headphones-alt playlist-icon"></i>
                 </div>
-            </a>
-            <div class="song_item song__options"> <a href="#"><i class="fas fa-ellipsis-v"></i></a></div>
-        </div>
-    </section>
+                <a href="#" class="song_item song__info">
+                    <div>
+                        <p class="song__title">Song Title 2</p>
+                        <p class="song__artist">Artist</p>
+                        <p class="song__time">3:00</p>
+                    </div>
+                </a>
+                <div class="song_item song__options"> <a href="#"><i class="fas fa-ellipsis-v"></i></a></div>
+            </div>
+        </section>
+    </div>
     <!--Create a new Playlist Modal-->
     <section id="createPlaylist">
         <div class="playlistForm">
@@ -100,7 +104,7 @@
     </section>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
-    <script src="scripts/playlists.js"></script>
+    <script src="../scripts/playlists.js"></script>
 </body>
 
 </html> 
