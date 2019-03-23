19b277e3654ac15ae330d35e731a9533b64f677e
Imzan Khan <ikhan5@lakeheadu.ca>
2019-03-22 20:54:47 -0400

    Started Validation on Edit Playlist

--- playlists/editplaylist.php
+++ playlists/editplaylist.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../model/database.php';
 require_once '../model/playlist_db.php';
-
+$errormsg ='';
 if(isset($_POST['editplaylist'])){
     $id = $_POST['playlistID'];
     $p = new PlaylistDB;
@@ -10,13 +10,18 @@ if(isset($_POST['editplaylist'])){
 
 if (isset($_POST['edit_playlist'])) {
     $p = new PlaylistDB();
-    $name = $_POST['name'];
-    $desc = $_POST['desc'];
+    $name = filter_input(INPUT_POST, 'name');
+    $desc = filter_input(INPUT_POST, 'desc', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $date = $_POST['date'];
     $playlist_id = $_POST['playlist_id'];
-    $p->editPlaylist($name, $desc, $date, $playlist_id);
-    header("Location: index.php");
-    exit;
+
+    if ($name == null || $desc == null) {
+        $errormsg = "Incomplete data. Please enter information in all fields.";
+    }
+    else{
+        $p->editPlaylist($name, $desc, $date, $playlist_id);
+        header("Location: index.php");
+    }
 }
 include "playlistHeader.php";
 ?>
@@ -26,9 +31,11 @@ include "playlistHeader.php";
         <i class="fas fa-arrow-left"> Back to Playlists</i>
     </a>
     <h2 class="heading-style2">Edit Playlist: </h2>
+    <div class="required">* Required Fields <span class="errorDiplay"><?=$errormsg?></span></div>
     <form method="post" action="">
         <input type="hidden" name="playlist_id" value="<?= htmlspecialchars($playlist->playlist_id); ?>" />
         <label for="name">Playlist Name: </label>
+        <span class="required">*</span>
         <input type="text" name="name" value='<?= htmlspecialchars($playlist->name) ?>'><br />
         <label for="name">Playlist Description: </label>
         <input type="text" name="desc" value='<?= htmlspecialchars($playlist->description) ?>'><br />
