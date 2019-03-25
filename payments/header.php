59ec0f03d67d322f103a2cddf748dc7730ca3c8a
Imzan Khan <ikhan5@lakeheadu.ca>
2019-03-25 15:06:43 -0400

    Added Session Variables, and added Different Views based on log in

---
+++ payments/header.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'startSession.php';
+require_once '../model/database.php';
+require_once '../model/payment_db.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Payment Portal</title>
+    <link rel="stylesheet" href="../CSS/payments.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+</head>
+
+<body>
