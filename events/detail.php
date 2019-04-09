9c159a1b3e3c0ea50b376c0c204a0d5356607171
Bibek <bibekmanshrestha@gmail.com>
2019-04-09 15:54:12 -0400

    add diff features link to events detail page

--- events/detail.php
+++ events/detail.php
@@ -10,17 +10,69 @@ include('header.php');
   <div class="row container justify-content-end">
     <a href=".?action=show_update_form&id=<?= $event->getId() ?>" class="btn btn-sm btn-outline-primary">Update</a>
     <a href="." class="btn btn-sm btn-outline-primary">Go to event list</a>
-    <a href="/carpool?eid=<?= $event->getId() ?>" class="btn btn-sm btn-outline-secondary">Carpool chat for the event</a>
   </div>
-  <div class="event-description container text-center pt-3 px-5"><?= $event->getDescription() ?></div>
-  <div class="container event-detail p-5">
-    <div class="d-flex justify-content-between">
-      <div class="ml-5">Venue: <em><?= $event->getLocation() ?></em></div>
-      <div class="mr-5">Date: <em><?= $event->getDate() ?></em></div>
+  <div id="event-detail-main" class="container">
+    <div class="event-description container text-center pt-3 px-3"><?= $event->getDescription() ?></div>
+    <div class="container event-detail p-3">
+      <div class="d-flex justify-content-between">
+        <div class="ml-5">Venue: <em><?= $event->getLocation() ?></em></div>
+        <div class="mr-5">Date: <em><?= $event->getDate() ?></em></div>
+      </div>
+      <div class="d-flex justify-content-between">
+        <div class="ml-5">Start Time: <em><?= $event->getStartTimeDisp() ?></em></div>
+        <div class="mr-5">End Time: <em><?= $event->getEndTimeDisp() ?></em></div>
+      </div>
     </div>
-    <div class="d-flex justify-content-between">
-      <div class="ml-5">Start Time: <em><?= $event->getStartTimeDisp() ?></em></div>
-      <div class="mr-5">End Time: <em><?= $event->getEndTimeDisp() ?></em></div>
+  </div>
+
+  <div class="card-deck mx-4 d-flex justify-content-center">
+    <div class="card col-1">
+      <img src="/Content/Images/guest_list.png" class="card-img-top" alt="..." >
+      <div class="card-body">
+        <h6 class="card-title text-center">Guest List</h6>
+      </div>
+    </div>
+    <div class="card col-1">
+      <img src="/Content/Images/to-do-list.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h6 class="card-title text-center">To-Do List</h6>
+      </div>
+    </div>
+    <div class="card col-1">
+      <img src="/Content/Images/food.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h6 class="card-title text-center">Food List</h6>
+      </div>
+    </div>
+    <div class="card col-1">
+      <img src="/Content/Images/drink.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h6 class="card-title text-center">Drink List</h6>
+      </div>
+    </div>
+    <div class="card col-1">
+      <img src="/Content/Images/poll.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h6 class="card-title text-center">Poll</h6>
+      </div>
+    </div>
+    <div class="card col-1">
+      <img src="/Content/Images/fund_pool.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h6 class="card-title text-center">Fund Pool</h6>
+      </div>
+    </div>
+    <div class="card col-1">
+      <img src="/Content/Images/car.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h6 class="card-title text-center">Carpool</h6>
+      </div>
+    </div>
+    <div class="card col-1">
+      <img src="/Content/Images/playlist.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h6 class="card-title text-center">Music Playlist</h6>
+      </div>
     </div>
   </div>
 
