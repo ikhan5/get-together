68a7cf2dadf86a25f08624dd5da53eb08385ed74
Bibek <bibekmanshrestha@gmail.com>
2019-04-16 11:25:35 -0400

    changes to events functionality

---
+++ errors/404Error.php
@@ -0,0 +1,10 @@
+<?php include($_SERVER['DOCUMENT_ROOT'].'/events/header.php');  ?>
+
+<div class="events__container">
+  <div class="container">
+    <h2>404</h2>
+    <span class="error_message">The page you are looking for couldn't be found.</span>
+  </div>
+</div>
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/events/footer.php') ?>
