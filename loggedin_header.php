c90aaed92fdb1624b703d4bc8e67e44e75310710
Bibek <bibekmanshrestha@gmail.com>
2019-04-18 18:33:11 -0400

    fix session start issue

--- loggedin_header.php
+++ loggedin_header.php
@@ -10,6 +10,8 @@
   <!-- Bootstrap links -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+    integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
   <link rel="stylesheet" href="/Content/style.css">
   <link rel="stylesheet" href="/Content/css/event.css">
   <link rel="stylesheet" href="/Content/css/account.css">
@@ -48,6 +50,7 @@
             <a class="nav-link navbar-custom" href="/">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
+            
             <?php if(isset($_SESSION['userid'])): ?>
               <div class="dropdown">
                 <a class="btn btn-primary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
